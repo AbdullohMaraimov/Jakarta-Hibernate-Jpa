0a01f605abb1c6b8bc0147dba854b72ca68262b6
Abdulloh Maraimov <laptop.2022@gmail.com>
2024-01-26 17:09:53 +0500

    Homework 8.2

--- src/main/java/servlets/GroupStudentHomeServlet.java
+++ src/main/java/servlets/GroupStudentHomeServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import com.mysql.jdbc.Driver;
+import jakarta.persistence.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -14,45 +15,67 @@ import java.util.List;
 
 @WebServlet(name = "GroupStudentHomeServlet", urlPatterns = "/group/student/*")
 public class GroupStudentHomeServlet extends HttpServlet {
-    String url = "jdbc:mysql://localhost:3306/demo";
-    String password = "student";
-    String user = "student";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pathInfo = request.getPathInfo();
         int groupId = Integer.parseInt(pathInfo.substring(1));
 
-        List<Student> studentList = new ArrayList<>();
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("orm_example");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-        try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            DriverManager.registerDriver(new Driver());
+        Query nativeQuery = entityManager.createNativeQuery("SELECT * FROM students WHERE group_id = :groupIdd", Student.class);
+        nativeQuery.setParameter("groupIdd", groupId);
+        List<Student> students = nativeQuery.getResultList();
 
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "select * from students where group_id = ? order by id"
-            );
-            preparedStatement.setInt(1, groupId);
-            ResultSet resultSet = preparedStatement.executeQuery();
+        request.setAttribute("students", students);
+        request.setAttribute("groupId", groupId);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/students.jsp");
+        requestDispatcher.forward(request, response);
 
-            while (resultSet.next()){
-                Student student = Student.builder()
-                        .id(resultSet.getInt("id"))
-                        .fullName(resultSet.getString("full_name"))
-                        .createdAt(resultSet.getTimestamp("created_at"))
-                        .groupId(resultSet.getInt("group_id"))
-                        .age(resultSet.getInt("age"))
-                        .build();
+        entityManager.getTransaction().commit();
 
-                studentList.add(student);
-            }
 
-            request.setAttribute("students", studentList);
-            request.setAttribute("groupId", groupId);
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/students.jsp");
-            requestDispatcher.forward(request, response);
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+//        String url = "jdbc:mysql://localhost:3306/demo";
+//        String password = "student";
+//        String user = "student";
+//        String pathInfo = request.getPathInfo();
+//        int groupId = Integer.parseInt(pathInfo.substring(1));
+//
+//        List<Student> studentList = new ArrayList<>();
+//
+//        try(Connection connection = DriverManager.getConnection(url, user, password)) {
+//            DriverManager.registerDriver(new Driver());
+//
+//            PreparedStatement preparedStatement = connection.prepareStatement(
+//                    "select * from students where group_id = ? order by id"
+//            );
+//            preparedStatement.setInt(1, groupId);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//
+//            while (resultSet.next()){
+//                Student student = Student.builder()
+//                        .id(resultSet.getInt("id"))
+//                        .fullName(resultSet.getString("full_name"))
+//                        .createdAt(resultSet.getTimestamp("created_at"))
+//                        .groupId(resultSet.getInt("group_id"))
+//                        .age(resultSet.getInt("age"))
+//                        .build();
+//
+//                studentList.add(student);
+//            }
+//
+//            request.setAttribute("students", studentList);
+//            request.setAttribute("groupId", groupId);
+//            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/students.jsp");
+//            requestDispatcher.forward(request, response);
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 
--- src/main/java/servlets/HomeServlet.java
+++ src/main/java/servlets/HomeServlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
 import com.mysql.jdbc.Driver;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -15,43 +18,59 @@ import java.util.List;
 @WebServlet(name = "servlets.HomeServlet", value = "")
 public class HomeServlet extends HttpServlet {
 
-    String url = "jdbc:mysql://localhost:3306/demo";
-    String password = "student";
-    String user = "student";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         List<Group> groupList = new ArrayList<>();
-        List<Student> studentList = new ArrayList<>();
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            DriverManager.registerDriver(new Driver());
 
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "select * from student_groups order by id"
-            );
-            ResultSet resultSet = preparedStatement.executeQuery();
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("orm_example");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-            while (resultSet.next()){
-                Group group = Group.builder()
-                        .id(resultSet.getInt("id"))
-                        .name(resultSet.getString("group_name"))
-                        .createdAt(resultSet.getTimestamp("created_at"))
-                        .build();
+        List<Group> groups = entityManager.createQuery("from Group t group by t.id", Group.class).getResultList();
 
-                groupList.add(group);
-            }
+        entityManager.getTransaction().commit();
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        request.setAttribute("groups", groupList);
+        request.setAttribute("groups", groups);
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/home.jsp");
         requestDispatcher.forward(request, response);
+
+//        String url = "jdbc:mysql://localhost:3306/demo";
+//        String password = "student";
+//        String user = "student";
+//
+//        List<Group> groupList = new ArrayList<>();
+//        List<Student> studentList = new ArrayList<>();
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//        try(Connection connection = DriverManager.getConnection(url, user, password)) {
+//            DriverManager.registerDriver(new Driver());
+//
+//            PreparedStatement preparedStatement = connection.prepareStatement(
+//                    "select * from student_groups order by id"
+//            );
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//
+//            while (resultSet.next()){
+//                Group group = Group.builder()
+//                        .id(resultSet.getInt("id"))
+//                        .name(resultSet.getString("group_name"))
+//                        .createdAt(resultSet.getTimestamp("created_at"))
+//                        .build();
+//
+//                groupList.add(group);
+//            }
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+
+//        request.setAttribute("groups", groupList);
+
     }
 
     @Override
